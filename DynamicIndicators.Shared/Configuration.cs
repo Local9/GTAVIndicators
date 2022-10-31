777ecfac7d48e4ff31c0ca555c42a3e1033f0519
local9 <apjcookie1@hotmail.com>
2022-10-31 01:11:51 +0000

    feat(config): added FiveM loading method

--- DynamicIndicators.Shared/Configuration.cs
+++ DynamicIndicators.Shared/Configuration.cs
@@ -1,11 +1,37 @@
-﻿using Newtonsoft.Json;
+﻿using DynamicIndicators.FiveM.Client;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace DynamicIndicators.Shared
 {
     internal class Configuration
     {
+        private static Configuration _configuration;
+
         [JsonProperty("vehicles")]
-        List<BlinkerParameters> Vehicles = new();
+        public List<BlinkerParameters> Vehicles = new();
+
+        internal static void Load()
+        {
+#if FIVEM
+            string configFile = LoadResourceFile(GetCurrentResourceName(), "config.json");
+            if (string.IsNullOrEmpty(configFile))
+            {
+                Main.Log.Error($"Failed to load config.json. Please inform the server owner that the config file is missing or corrupt.");
+                return;
+            }
+            _configuration = JsonConvert.DeserializeObject<Configuration>(configFile);
+#endif
+        }
+
+#if FIVEM
+        public static Configuration GetConfig()
+        {
+            if (_configuration is null)
+                Load();
+            
+            return _configuration;
+        }
+#endif
     }
 }
