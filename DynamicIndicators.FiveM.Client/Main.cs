d36b3bd96a43b76c6dd52db541054e119c1814a0
local9 <apjcookie1@hotmail.com>
2022-10-31 01:12:33 +0000

    feat(main): load config

--- DynamicIndicators.FiveM.Client/Main.cs
+++ DynamicIndicators.FiveM.Client/Main.cs
@@ -1,9 +1,17 @@
-﻿using CitizenFX.Core;
+﻿using DynamicIndicators.Shared;
+using System.Collections.Generic;
 
 namespace DynamicIndicators.FiveM.Client
 {
     public class Main : BaseScript
     {
+        List<BlinkerParameters> _blinkerParameters = new();
+        public static Log Log = new();
 
+        public Main()
+        {
+            _blinkerParameters = Configuration.GetConfig().Vehicles;
+            
+        }
     }
 }
