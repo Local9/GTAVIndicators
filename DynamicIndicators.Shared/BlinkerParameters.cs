f74611332ed352d0808382b5fac9056a96cedf96
local9 <apjcookie1@hotmail.com>
2022-10-31 01:11:02 +0000

    refactor(blinker): duration cannot go below zero

--- DynamicIndicators.Shared/BlinkerParameters.cs
+++ DynamicIndicators.Shared/BlinkerParameters.cs
@@ -4,11 +4,20 @@ namespace DynamicIndicators.Shared
 {
     internal class BlinkerParameters
     {
+        private int _duration;
+
         [JsonProperty("model")]
         public string ModelName;
 
         [JsonProperty("duration")]
-        public int Duration;
+        public int Duration
+        {
+            get => _duration;
+            set
+            {
+                _duration = value < 0 ? 0 : value;
+            }
+        }
 
         [JsonProperty("debug")]
         public bool Debug;
