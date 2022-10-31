1b08be20a3f0b08b89ddaedf099c0a30e907ea1c
local9 <apjcookie1@hotmail.com>
2022-10-31 00:35:52 +0000

    refactor(files): move files

---
+++ FiveM-Resource-Files/fxmanifest.lua
@@ -0,0 +1,12 @@
+game "gta5"
+fx_version "cerulean"
+
+author "Local9 <https://github.com/Local9>, ikt <https://github.com/E66666666>, tk0wnz <https://github.com/tk0wnz>"
+description "Dynamic Indicators converted for FiveM by Local9"
+version "1.0.0"
+
+files {
+	"Newtonsoft.Json.dll"
+}
+
+client_script "*.net.dll"
